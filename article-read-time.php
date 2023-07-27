af9c8cb220b94bf3bccd780aa3fb01f3e9888034
melodieot <melodie.olory@gmail.com>
2023-07-27 10:44:22 +0200

    second commit

---
+++ article-read-time.php
@@ -0,0 +1,16 @@
+<?php
+/*
+Plugin Name: Article Read Time
+Plugin URI: https://github.com/Melodiecoding/article-read-time
+Description: Une fonctionnalité à votre siteWordPress sur mesure qui calculera automatiquement le temps de lecture d'un article à son enregistrement.
+Version: 0.0.1
+Author: Melodiecoding
+Author URI: https://github.com/Melodiecoding
+Text Domain: article-read-time
+License: GPLv3
+License URI: http://www.gnu.org/licenses/gpl.html
+*/
+
+
+
+
