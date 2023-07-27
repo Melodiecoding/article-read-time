889559c186727e3fd304dcf9e10f2cce793fe898
Nesami Haiiro <20476281+citrusMarmelade@users.noreply.github.com>
2023-07-27 10:46:42 +0200

    Reprise du code de calcul de mots par minutes

--- article-read-time.php
+++ article-read-time.php
@@ -11,6 +11,24 @@ License: GPLv3
 License URI: http://www.gnu.org/licenses/gpl.html
 */
 
+const WORDS_PER_MINUTE = 250;
+const READING_TIME_META_KEY = 'reading_time';
 
+function add_reading_time($post_id, WP_Post $post, bool $update) {
+    // Do not execute when the add button is used
+    if (!$update) { return; }
+    // Do not execute when the post is a revision
+    if (wp_is_post_revision($post_id)) { return; }
+    // Do not execute on autosaves
+    if (defined('DOING_AUTOSAVE') and DOING_AUTOSAVE) { return; }
+    // Only for articles
+    if ($post->post_type != 'post') { return; }
 
+    $word_count = str_word_count(strip_tags($post->post_content));
 
+    $minutes = ceil($word_count/WORDS_PER_MINUTE);
+
+    update_post_meta($post_id, READING_TIME_META_KEY, $minutes);
+}
+
+add_action('save_post', 'add_reading_time', accepted_args: 3);
